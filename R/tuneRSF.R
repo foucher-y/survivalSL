22a58c0658753b106de11d13d339750eebe5ed18
belkebir-mekki2000 <aminabelkebirmekki@gmail.com>
2025-04-04 15:39:11 +0200

    Update tuneRSF.R

--- R/tuneRSF.R
+++ R/tuneRSF.R
@@ -1,36 +1,66 @@
+tuneRSF <- function(formula, data, nodesize, mtry, ntree, seed=NULL){
 
-tuneRSF <- function(times, failures, group=NULL, cov.quanti=NULL,
-                         cov.quali=NULL, data, nodesize, mtry, ntree){
 
-  .outcome <- paste("Surv(", times, ",", failures, ")")
+  if(any(sapply(data,is.character)))stop("Error : some columns are of type character. Only numeric or factor variables are allowed.")
 
-  if(!(is.null(group))){
-    if(is.null(cov.quanti)==F & is.null(cov.quali)==F){
-      .f <- as.formula( paste(.outcome, "~", group, "+", paste( cov.quanti,  collapse = " + "), " + ", paste(cov.quali, collapse = " + "),
-                              collapse = " ") )
-    }
-    if(is.null(cov.quanti)==F & is.null(cov.quali)==T){
-      .f <- as.formula( paste(.outcome, "~", group, "+", paste( cov.quanti, collapse = " + "),collapse = " ") )
-    }
-    if(is.null(cov.quanti)==T & is.null(cov.quali)==F){
-      .f <- as.formula( paste(.outcome, "~", group, "+",paste(cov.quali, collapse = " + "),collapse = " ") )
-    }
-    if(is.null(cov.quanti)==T & is.null(cov.quali)==T){
-      .f <- as.formula( paste(.outcome, "~", group) )
-    }
+
+  if(is.null(seed)){
+    seed<-sample(1:1000,1)
   }
-  else{
-    if(is.null(cov.quanti)==F & is.null(cov.quali)==F){
-      .f <- as.formula( paste(.outcome, "~", paste( cov.quanti,  collapse = " + "), " + ", paste(cov.quali, collapse = " + "),
-                              collapse = " ") )
-    }
-    if(is.null(cov.quanti)==F & is.null(cov.quali)==T){
-      .f <- as.formula( paste(.outcome, "~", paste( cov.quanti, collapse = " + "),collapse = " ") )
-    }
-    if(is.null(cov.quanti)==T & is.null(cov.quali)==F){
-      .f <- as.formula( paste(.outcome, "~",  paste(cov.quali, collapse = " + "),collapse = " ") )
-    }
+
+  if (missing(formula)) stop("The 'formula' argument is required.")
+  if (missing(data)) stop("The 'data' argument is required.")
+  if (missing(nodesize)) stop("The 'nodesize' argument is required.")
+  if (missing(mtry)) stop("The 'mtry' argument is required.")
+  if (missing(ntree)) stop("The 'ntree' argument is required.")
+
+
+  variables_formula <- all.vars(formula)
+
+  times <- variables_formula[1]
+  failures <- variables_formula[2]
+
+
+  if("." %in% variables_formula){
+    vars<-setdiff(names(data),c(times,failures))
+    .outcome <- paste("Surv(", times, ",", failures, ")")
+    formula <- as.formula(paste(.outcome, "~", paste(vars, collapse = " + ")))
+    variables_formula <- all.vars(formula)
   }
+
+
+  variables_existent <- all(variables_formula %in% names(data))
+  if (!variables_existent) stop("One or more variables from the formula do not exist in the data.")
+
+  rm(variables_existent)
+
+
+  is_binary <- all(data[[failures]] %in% c(0, 1))
+
+  if (! is_binary) stop("The 'failures' variable is not coded as 0/1.")
+
+  rm(is_binary)
+
+  all_terms <- attr(terms(formula), "term.labels")
+  strata_terms <- grep("strata\\(", all_terms, value = TRUE)
+  ns_terms <- grep("ns\\(", all_terms, value = TRUE)
+  bs_terms <- grep("bs\\(", all_terms, value = TRUE)
+  if(length(strata_terms) >= 1) stop("The 'randomForestSRC' package does not support the use of 'strata()' in the formula.")
+
+
+  if((length(ns_terms) >= 1)|(length(bs_terms) >= 1)|(length(all_terms)>(length(variables_formula)-2))){
+    vars<-setdiff(variables_formula,c(times,failures))
+    .outcome <- paste("Surv(", times, ",", failures, ")")
+    formula <- as.formula(paste(.outcome, "~", paste(vars, collapse = " + ")))
+  }
+  rm(all_terms,strata_terms,ns_terms,bs_terms)
+
+  if (any(is.na(data[,variables_formula]))){
+    subset_data<-na.omit(data[,variables_formula])
+    data<-cbind(subset_data, data[!colnames(data) %in% colnames(subset_data), drop = FALSE])
+    warning("Data need to be without NA. NA is removed")
+  }
+
 
   old <- options()
   on.exit(options(old))
@@ -39,8 +69,8 @@ tuneRSF <- function(times, failures, group=NULL, cov.quanti=NULL,
 
   find.tune.rf.fast<-function(param.test, f, data ){
 
-    res.rsf <- rfsrc(f, data = data, nodesize = param.test[1], mtry = param.test[2],
-                          ntree = param.test[3], splitrule="logrank")
+    res.rsf <- rfsrc(f, data = data, nodesize = param.test[1], mtry = param.test[2] ,
+                     ntree = param.test[3] , seed=-seed, splitrule="logrank")
 
     res<-c(param.test[1], param.test[2], param.test[3],  tail(res.rsf$err.rate, 1))
 
@@ -51,7 +81,7 @@ tuneRSF <- function(times, failures, group=NULL, cov.quanti=NULL,
   .grid=cbind(.grid[,1],.grid[,2], .grid[,3])
 
 
-  .tune.rf<-apply(.grid,MARGIN=1, FUN=find.tune.rf.fast, f=.f, data=data)
+  .tune.rf<-apply(.grid,MARGIN=1, FUN=find.tune.rf.fast, f=formula, data=data)
 
   .res=t(.tune.rf)
   colnames(.res)=c("nodesize","mtry","ntree","error")
@@ -61,9 +91,10 @@ tuneRSF <- function(times, failures, group=NULL, cov.quanti=NULL,
   .mini<-.mini[1,]
 
   .optimal=list(nodesize=as.numeric(.mini$nodesize),
-               mtry=as.numeric(.mini$mtry),
-               ntree=as.numeric(.mini$ntree))
+                mtry=as.numeric(.mini$mtry),
+                ntree=as.numeric(.mini$ntree))
 
-  return(list(optimal=.optimal, results = .tune.rf))
+  return(list(optimal=.optimal, results = t(.tune.rf)))
 }
+
 
